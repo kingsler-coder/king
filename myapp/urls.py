f5968e2c8d7a01b0812c58d6e418637aee917ccf
kingsler-coder <emmaculatemuthoni75@gmail.com>
2025-03-04 14:04:39 +0300

    update

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
 path('',views.User_list),
-path('Add/',views.AddUser)
+path('Add/',views.AddUser),
+    path('Edit/<id>',views.EditUser),
+    path('Delete/<eid>',views.DeleteUser),
+    path('View/<eid>',views.ViewUser)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -13,7 +13,23 @@ def AddUser(request):
         return redirect('/')
     mydict['form'] = form
     return render(request,'add.html',mydict)
-
-
+def EditUser(request,id=None):
+    one_rec=User.objects.get(pk=id)
+    form = UserForm(request.POST or None, request.FILES or None, instance=one_rec)
+    if form.is_valid():
+        form.save()
+        return  redirect('/')
+    return render(request, 'edit.html',{'form':form})
+def DeleteUser(request,eid=None):
+    one_rec=User.objects.get(pk=eid)
+    if request.method=='POST':
+        one_rec.delete()
+        return redirect('/')
+    return render(request,'delete.html')
+def ViewUser(request,eid=None):
+    mydict = {}
+    one_rec=User.objects.get(pk=eid)
+    mydict['user'] = one_rec
+    return render(request,'view.html',mydict)
 
 
